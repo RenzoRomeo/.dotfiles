219fad651e378e35e1ff3894374c94648512bada
root <root@DESKTOP-BFS7AV2>
2023-08-18 21:15:01 -0300

    feat: new keymaps for lsp

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,11 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- LSP
+map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+map("n", "gr", "<cmd>lua vim.lsp.buf.rename()<cr>")
+map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+
 -- Telescope
 map("n", "<leader>sf", "<cmd>Telescope find_files<cr>")
 map("n", "<leader>sg", "<cmd>Telescope live_grep<cr>")
