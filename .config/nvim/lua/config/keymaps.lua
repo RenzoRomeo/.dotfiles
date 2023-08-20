bf551030ee0671b5906ee65c632749dd21d2c56d
root <root@DESKTOP-BFS7AV2>
2023-08-20 18:32:33 -0300

    feat: show hidden files in telescope file search

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,7 +7,7 @@ local function map(mode, lhs, rhs, opts)
 end
 
 -- Telescope
-map("n", "<leader>sf", "<cmd>Telescope find_files<cr>")
+map("n", "<leader>sf", "<cmd>Telescope find_files hidden=true<cr>")
 map("n", "<leader>sg", "<cmd>Telescope live_grep<cr>")
 map("n", "<leader>sb", "<cmd>Telescope buffers<cr>")
 
